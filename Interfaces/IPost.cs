596091da527a76d683567f82c3cfc18abc1d4127
Matheus Martins <matheusmartinsnascimento@gmail.com>
2021-07-02 17:35:57 -0300

    feito PostModel, IPost, e parcialmente PostContoller, necessário o Login a partir daqui

--- Interfaces/IPost.cs
+++ Interfaces/IPost.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using Instadev.Models;
+
 namespace Instadev.Interfaces
 {
     public interface IPost
     {
-         
+        void Criar(Post P);
+        List<Post> LerTodas();
+        void Alterar(Post P);
+        void Deletar(int ID);
     }
 }
--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Instadev.Interfaces;
@@ -18,7 +19,7 @@ namespace Instadev.Models
         }
         public string Preparar(Post P)
         {
-            return $"";
+            return $"{P.IDPost};{P.Imagem};{P.Legenda};{P.Likes};{P.IDUsuario}";
         }
 
         public void Alterar(Post P)
@@ -46,10 +47,18 @@ namespace Instadev.Models
         {
             List<Post> Posts = new List<Post>();
             string[] Linhas = File.ReadAllLines(CAMINHO);
+
             foreach (var item in Linhas)
             {
+                string[] linha = item.Split(";");
                 Post p = new Post();
-                
+                p.IDPost = Int32.Parse(linha[0]);
+                p.Imagem = linha[1];
+                p.Legenda = linha[2];
+                p.Likes = Int32.Parse(linha[3]);
+                p.IDUsuario = Int32.Parse(linha[4]);
+
+                Posts.Add(p);
             }
             return Posts;
         }
