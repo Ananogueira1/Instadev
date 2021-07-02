936916a7869a79a28a1cb6c6a5194c13283e6844
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-07-02 18:34:54 -0300

    controller e view de usuario em processo, login e model ok. necessario teste e tirar duvidas

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,7 +1,38 @@
+using System;
+using Instadev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Instadev.Controllers
 {
-    public class UsuarioController
+         [Route("Usuario")]
+    public class UsuarioController : Controller
     {
-        
+        Usuario usuarioModel = new Usuario();
+
+        [Route("Informações")]
+
+        public IActionResult Index()
+        {
+            ViewBag.Usuarios = usuarioModel.ExibirInfo();
+            return View();
+        }
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Usuario novoUsuario = new Usuario();
+            
+            novoUsuario.ImagemDePerfil = form["Imagem de perfil"];
+            novoUsuario.IdUsuario = Int32.Parse(form["IdUsuario"]);
+            novoUsuario.Nome = form["Nome"];
+            novoUsuario.NomeDeUsuario = form["Nome de Usuário"];
+            novoUsuario.Email = form["Email"];
+            // novoUsuario.Senha = form["Senha"];
+
+            usuarioModel.Cadastrar(novoUsuario);
+            ViewBag.Usuarioes = usuarioModel.ExibirInfo();
+
+            return LocalRedirect("~/Usuario/Listar");
+        }
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,7 +1,75 @@
+using System.Collections.Generic;
+using System.IO;
+using Instadev.Interfaces;
+
 namespace Instadev.Models
 {
-    public class Usuario
+    public class Usuario : InstadevBase , IUsuario 
     {
-        
+        public int IdUsuario { get; set; }
+        public string Email;
+        private string Senha;
+        public string Nome { get; set; }
+
+        public string NomeDeUsuario { get; set; }
+
+        // List<Post> Postagem = new List<Post>();
+
+        public string ImagemDePerfil { get; set; }
+        private const string CAMINHO = "Database/Usuario.csv";
+
+          public Usuario()
+        {
+            CriarPastaEArquivo(CAMINHO);
+        }
+
+        private string PrepararLinha(Usuario u)
+        {
+            return $"{u.ImagemDePerfil};{u.IdUsuario};{u.Nome};{u.NomeDeUsuario};{u.Email};{u.Senha}";
+        }
+
+        public void Cadastrar(Usuario u)
+        {
+            string[] linha = { PrepararLinha(u) };
+            File.AppendAllLines(CAMINHO, linha);
+        }
+
+        public List<Usuario> ExibirInfo()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            string[] linhas = File.ReadAllLines(CAMINHO);
+
+            foreach (var item in linhas)
+            {
+                string[] linha = item.Split(";");
+
+                Usuario usuario = new Usuario();
+                usuario.ImagemDePerfil = linha [0];
+                usuario.IdUsuario = int.Parse(linha[1]);
+                usuario.Nome = linha[2];
+                usuario.NomeDeUsuario = linha[3];
+                usuario.Email = linha[4];
+                usuario.Senha = linha[5];
+
+                usuarios.Add(usuario);
+            }
+            return usuarios;
+        }
+
+        public void EditarPerfil(Usuario u)
+        {
+            List<string> linhas = LerTodasLinhasCSV(CAMINHO);
+            linhas.RemoveAll(x => x.Split(";")[0] == u.IdUsuario.ToString());
+            linhas.Add(PrepararLinha(u) );                        
+            ReescreverCSV(CAMINHO, linhas); 
+        }
+
+        public void DeletarPerfil(int id)
+        {
+            List<string> linhas = LerTodasLinhasCSV(CAMINHO);
+            linhas.RemoveAll(x => x.Split(";")[0] == IdUsuario.ToString());                        
+            ReescreverCSV(CAMINHO, linhas);
+        }
     }
+
 }
