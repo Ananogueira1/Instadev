b731bc24e55854f7c554c9f877d4c2a0735d7bbb
PedroLCamara <plcrlschool@gmail.com>
2021-07-06 18:45:27 -0300

    EditarUsuarioPreTestes

--- Controllers/EditarPerfilController.cs
+++ Controllers/EditarPerfilController.cs
@@ -16,30 +16,6 @@ namespace Instadev.Controllers
         {
             return View();
         }
-        [Route("AlterarFoto")]
-        public IActionResult AlterarFoto(IFormCollection Form)
-        {
-            List<Usuario> Usuarios = UsuarioModel.ExibirInfo();
-            Usuario NovoUsuario = Usuarios.Find(x => x.NomeDeUsuario == ViewBag.Username);
-
-            if (Form.Files.Count > 0)
-            {
-                var Arquivo = Form.Files[0];
-                var Pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Usuarios");
-                if (!Directory.Exists(Pasta))
-                {
-                    Directory.CreateDirectory(Pasta);
-                }
-                var caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Usuarios", Arquivo.FileName);
-                using (var Stream = new FileStream(caminho, FileMode.Create))
-                {
-                    Arquivo.CopyTo(Stream);
-                }
-                NovoUsuario.ImagemDePerfil = Arquivo.FileName;
-            }
-            UsuarioModel.EditarPerfil(NovoUsuario);
-            return LocalRedirect("~/EditarPerfil/Index");
-        }
         [Route("AlterarUsuario")]
         public IActionResult AlterarUsuario(IFormCollection Form)
         {
@@ -60,7 +36,30 @@ namespace Instadev.Controllers
             {
                 NovoUsuario.NomeDeUsuario = Usuarios.Find(x => x.NomeDeUsuario == ViewBag.Username).NomeDeUsuario;
             }
+            if (Form.Files.Count > 0)
+            {
+                var Arquivo = Form.Files[0];
+                var Pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Usuarios");
+                if (!Directory.Exists(Pasta))
+                {
+                    Directory.CreateDirectory(Pasta);
+                }
+                var caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Usuarios", Arquivo.FileName);
+                using (var Stream = new FileStream(caminho, FileMode.Create))
+                {
+                    Arquivo.CopyTo(Stream);
+                }
+                NovoUsuario.ImagemDePerfil = Arquivo.FileName;
+            }
+            else{
+                NovoUsuario.ImagemDePerfil = Usuarios.Find(x => x.NomeDeUsuario == ViewBag.Username).ImagemDePerfil;
+            }
             return LocalRedirect("~/EditarPerfil/Index");
+        }
+        public IActionResult DeletarUsuario(){
+            UsuarioModel.DeletarPerfil(UsuarioModel.ExibirInfo().Find(x => x.NomeDeUsuario == ViewBag.Username).IdUsuario);
+            ViewBag.Username = null;
+            return LocalRedirect("~/Home/Index");
         }
     }
 }
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Instadev.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Instadev.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // ViewBag.Username = HttpContext.Session.GetString("Username");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
